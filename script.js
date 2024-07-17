c3a876cc07cd39724b0537133da77ff785d3a0d8
Amit <amit24ve@gmail.com>
2024-07-17 13:42:47 +0530

    commit

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Document loaded");
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const contactButton = document.getElementById("contactButton");
+    const contactSection = document.getElementById("Contact");
+
+    contactButton.addEventListener("click", function() {
+        contactSection.scrollIntoView({ behavior: "smooth" });
+    });
+});
